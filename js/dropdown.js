ae141576676c3db3f9942c3a244d7aa38c3bf8f6
Pinkmann <korgov.vova@icloud.com>
2021-03-04 10:00:42 +0200

    cars

---
+++ js/dropdown.js
@@ -0,0 +1,38 @@
+class Dropdown {
+    constructor() {
+        this.wrapper = document.querySelector('[data-wrapper]');
+        this.background = document.getElementById('dropdown-background');
+        wrapper.addEventListener('click', this.handleWrapperClick);
+    }
+
+    handleWrapperClick = (e) => {
+        e.preventDefault;
+        let link = e.target.closest('[data-link]');
+
+        if (link) {
+            let item = e.target.closest('[data-item]');
+            let dropdown = item.querySelector(' [data-item] > [data-dropdown]');
+            dropdown.classList.add('opened');
+            this.background.classList.add('opened');
+        }
+
+        let list = e.target.closest('[data-submenu-list]');
+        let item = e.target.closest('[data-submenu-item]');
+
+        if (item) {
+            let submenu = item.querySelector('[data-submenu-item] > [data-submenu]');
+            let items = list.querySelectorAll('.dropdown__list > .dropdown__item');
+            items.forEach(item => {
+                item.classList.remove('opened');
+            });
+            item.classList.add('opened');
+        }
+
+        let close = e.target.closest('[data-close]');
+        if (close) {
+            let wrapper = close.closest('[data-dropdown]');
+            this.background.classList.remove('opened');
+            wrapper.classList.remove('opened');
+        }   
+    } 
+}
