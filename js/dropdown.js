7292fb7c3af4de5b8cafdf2314a9b7ec6d6399e0
Pinkmann <korgov.vova@icloud.com>
2021-03-11 22:31:51 +0200

    final

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,6 +4,7 @@ class Dropdown {
         this.handleBgClick = this.handleBgClick.bind(this);
         this.wrapper = document.querySelector('[data-wrapper]');
         this.background = document.getElementById('dropdown-background');
+        this.headerOuter = document.getElementById('header-outer');
         wrapper.addEventListener('click', this.handleWrapperClick); 
         this.background.addEventListener('click', this.handleBgClick); 
     }
@@ -29,8 +30,11 @@ class Dropdown {
             let dropdown = item.querySelector(' [data-item] > [data-dropdown]');
             dropdown.classList.add('opened');
             this.background.classList.add('opened');
+            setTimeout(() => {
+                this.headerOuter.classList.add('index');
+            }, 500)
             document.body.style.overflow = 'hidden';
-           
+            
             if(window.matchMedia("(pointer: coarse)").matches) {
                 document.body.style.marginRight = '0';
             } else {
@@ -55,6 +59,7 @@ class Dropdown {
             let wrapper = close.closest('[data-dropdown]');
             this.background.classList.remove('opened');
             wrapper.classList.remove('opened');
+            this.headerOuter.classList.remove('index');
             document.body.style.overflow = null;
             document.body.style.marginRight = null;
         }   
